373be4477edd93717929bf779bc9351f93a0db46
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-08-24 13:59:05 +0530

    Adding the nav bar in glass and blured form

--- client/src/components/Career.js
+++ client/src/components/Career.js
@@ -9,17 +9,17 @@ export default function Career() {
                         <h2 className="pt-[50px] pb-[20px] text-center text-[2rem] font-[600] font-serif text-[#0095ff]">Career services</h2>
                         <section className="list-none overflow-y-hidden">
                             <div className="flex">
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul className="text1">
                                         <h3 className="text-center text-[#0095ff]"> Placement Assistence</h3>
                                     </ul>
                                 </div>
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul>
                                         <h3 className="text-center text-[#0095ff]">Complete Online education</h3>
                                     </ul>
                                 </div>
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul>
                                         <h3 className="text-center text-[#0095ff]">One on One doubt sessions</h3>
                                     </ul>
@@ -28,17 +28,17 @@ export default function Career() {
                         </section>
                         <section className="list-none overflow-y-hidden">
                             <div className="flex">
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul className="text1">
                                         <h3 className="text-center text-[#0095ff]">Communities</h3>
                                     </ul>
                                 </div>
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul>
                                         <h3 className="text-center text-[#0095ff]">Certificates</h3>
                                     </ul>
                                 </div>
-                                <div className="m-[5px] p-[25px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
+                                <div className="m-[5px] p-[40px] h-[35vh] w-[30vw] flex flex-col justify-end rounded-[20px] rounded-tr-[0px] rounded-bl-[0px] border-[3px] border-[#0095ff] border-solid">
                                     <ul>
                                         <h3 className="text-center text-[#0095ff]">Group Projects</h3>
                                     </ul>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import './Navbar.css'
 export default function Navbar() {
     return (
         <>
             <div className="relative">
-                <div className="fixed top-0 left-0 right-0">
+                <div className="fixed top-0 left-0 right-0 backdrop-blur-[10px] border-b-2 border-[#00000033]">
                     <div className="container mx-auto">
-                        <nav className='flex bg-white justify-between'>
-                            <div className="text-[#0095ff] flex text-sm font-bold md:text-2xl font-sans p-3 sm:text-xl">Knowly!</div>
+                        <nav className='flex bg-transparent box-border justify-between'>
+                            <div className="text-[#0095ff] flex text-sm font-bold md:text-2xl font-sans p-3 sm:text-xl"><a href="/">Knowly!</a></div>
                             <ul className='flex text-sm items-center text-[#0095ff]'>
                                 <li><a className='p-3' href="#Home">Home</a></li>
-                                <li><a className='p-3' href="#Courses">Courses</a></li>
-                                <li><a className='p-3' href="#Communities">Communities</a></li>
-                                <li><a className='p-3' href="#Career">Career</a></li>
-                                <li><a className='p-3' href="#Features">Features</a></li>
-                                <li><a className='p-3' href="#Contactus">ContactUs</a></li>
-                                <a className='border-[1px] border-solid border-[#000] rounded-full px-2 bg-[#0095ff] text-[#fff] hover:bg-[#fff] hover:text-[#0095ff] hover:px-3 hover:py-1' href="/signin" target="_blank"><li><button>Sign-In</button></li></a>
+                                <li><a className='p-3 ' href="#Courses">Courses</a></li>
+                                <li><a className='p-3 ' href="#Communities">Communities</a></li>
+                                <li><a className='p-3 ' href="#Career">Career</a></li>
+                                <li><a className='p-3 ' href="#Features">Features</a></li>
+                                <li><a className='p-3 ' href="#Contactus">ContactUs</a></li>
+                                <a className='border-[1px] border-solid border-[#000] rounded-full bg-[#0095ff] text-[#fff] hover:bg-[#fff] hover:text-[#0095ff] px-3 py-1' href="/signin" target="_blank"><li><button>Sign-In</button></li></a>
                             </ul>
                         </nav>
                     </div>
